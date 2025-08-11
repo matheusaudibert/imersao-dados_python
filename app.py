a543980dfdd04e987828448e951d504a66fa40e8
Matheus Audibert <matheusaudibert2019@outlook.com>
2025-08-10 23:38:25 -0300

    streamlit init

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# --- Configuração da Página ---
+# Define o título da página, o ícone e o layout para ocupar a largura inteira.
+st.set_page_config(
+    page_title="Dashboard de Salários na Área de Dados",
+    page_icon="📊",
+    layout="wide",
+)
